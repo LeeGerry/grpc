8abb2100789c2b0ae5dc1d6794796e5ea0fb1c9b
gerry <gerry>
2023-06-08 17:22:14 -0700

    Add handling error

--- src/main/java/com/me/server/BankService.java
+++ src/main/java/com/me/server/BankService.java
@@ -1,6 +1,7 @@
 package com.me.server;
 
 import com.me.models.*;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
@@ -21,15 +22,17 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
         int accountNumber = request.getAccountNumber();
         int amount = request.getAmount();
         int balance = AccountDatabase.getBalance(accountNumber);
+        // https://developers.google.com/maps-booking/reference/grpc-api-v2/status_codes
+        if (balance < amount) {
+            Status status = Status.FAILED_PRECONDITION.withDescription("No enough money! You have only " + balance);
+            responseObserver.onError(status.asRuntimeException());
+            return;
+        }
+        // all the validations passed
         for (int i = 0; i < amount / 10; i++) {
             Money money = Money.newBuilder().setValue(10).build();
             responseObserver.onNext(money);
             AccountDatabase.deductBalance(accountNumber, 10);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }
         responseObserver.onCompleted();
     }
