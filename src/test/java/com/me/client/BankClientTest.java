f5b5f93af9346e8ea656befc9c866d0bdf30d003
gerry <gerry>
2023-06-08 21:00:01 -0700

    增加Server Streaming Client Async实现

--- src/test/java/com/me/client/BankClientTest.java
+++ src/test/java/com/me/client/BankClientTest.java
@@ -1,16 +1,21 @@
 package com.me.client;
 
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.me.models.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.concurrent.TimeUnit;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BankClientTest {
     private BankServiceGrpc.BankServiceBlockingStub blockingStub;
+    private BankServiceGrpc.BankServiceStub bankServiceStub;
 
     @BeforeAll
     public void setup() {
@@ -19,6 +24,7 @@ public class BankClientTest {
                 .usePlaintext()
                 .build();
         blockingStub = BankServiceGrpc.newBlockingStub(channel);
+        bankServiceStub = BankServiceGrpc.newStub(channel);
     }
 
     @Test
@@ -37,5 +43,27 @@ public class BankClientTest {
                     System.out.println("result: " + value);
                     Assertions.assertEquals(10, value);
                 });
+    }
+
+    @Test
+    public void withdrawAsyncTest() {
+        WithdrawRequest request = WithdrawRequest.newBuilder().setAccountNumber(8).setAmount(40).build();
+        bankServiceStub.withdraw(request, new StreamObserver<Money>() {
+            @Override
+            public void onNext(Money money) {
+                System.out.println("received: " + money.getValue());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("error: " + throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("server is done!");
+            }
+        });
+        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
     }
 }
