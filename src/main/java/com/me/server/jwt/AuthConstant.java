7c892eab2bb20ff3158a47df6f323af3881e14e6
gerry <gerry>
2023-07-23 01:48:12 -0700

    修改秘钥长度

--- src/main/java/com/me/server/jwt/AuthConstant.java
+++ src/main/java/com/me/server/jwt/AuthConstant.java
@@ -7,7 +7,7 @@ import javax.crypto.SecretKey;
 
 public interface AuthConstant {
     // JWT秘钥
-    SecretKey JWT_KEY = Keys.hmacShaKeyFor("hello_grpc_666".getBytes());
+    SecretKey JWT_KEY = Keys.hmacShaKeyFor("hello_grpc_666_adfdfaif_addd333fffsdfasdf".getBytes());
     // 客户端ID，即客户端发送来的请求携带了JWT字符串，通过JWT字符串确认了用户身份，就存在这个变量中
     Context.Key<String> AUTH_CLIENT_ID = Context.key("clientId");
     // 携带JWT字符串的请求头的KEY
